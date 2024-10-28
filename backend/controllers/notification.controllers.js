357081479a1cfd89491be72e183d8a749b06a58f
guddu-paul <guddu.paul@dotsquares.com>
2024-10-28 20:38:51 +0530

    ui design complete

--- backend/controllers/notification.controllers.js
+++ backend/controllers/notification.controllers.js
@@ -26,3 +26,24 @@ export const deleteNotifications = async (req , res) => {
         return res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const deleteNotification = async (req , res) => {
+    try {
+        const notificationId = req.params.id;
+
+        const userId = req.user._id;
+
+        const notification = await Notification.findById(notificationId);
+
+        if(!notification) return res.status(404).json({ message: "Notification not found"});
+
+        if(notification.to.toString() !== userId.toString()) return res.status(404).json({ message: "You are not allowed to delete this notification" });
+
+        await Notification.findByIdAndDelete(notificationId);
+
+        return res.status(200).json({ message: "Notification deleted successfully"});
+    } catch (error) {
+        console.log("error in deleting post controller",error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { deleteNotifications, getNotifications } from "../controllers/notification.controllers.js";
+import { deleteNotification, deleteNotifications, getNotifications } from "../controllers/notification.controllers.js";
 
 const router = express.Router();
 
 router.get('/', protectRoute, getNotifications);
 router.delete('/', protectRoute, deleteNotifications);
+router.delete('/:id', protectRoute, deleteNotification); //TODO
 
 export default router;
