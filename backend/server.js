a3c9db02b3d9f0b0444154a11f7758fa656da685
guddu-paul <guddu.paul@dotsquares.com>
2024-11-14 20:51:20 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary} from "cloudinary";
+import path from 'path';
 
 import cors from 'cors';
 
@@ -23,6 +24,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5003;
 
+const __dirname = path.resolve()
+
 app.use(cors());
 app.use(express.json({limit: "3mb"})); // to parse req body;
 app.use(express.urlencoded({ extended: true})); //to parse formdata
@@ -33,6 +36,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log('server is running on', PORT);
